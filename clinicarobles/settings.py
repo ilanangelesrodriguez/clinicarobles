d26079dfea2cf60063b0f191c34ec593310df7d5
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2024-11-12 12:44:23 +0000

    feat: agregar nueva URL a CSRF_TRUSTED_ORIGINS y añadir numpy a los requisitos

--- clinicarobles/settings.py
+++ clinicarobles/settings.py
@@ -94,6 +94,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://localhost:8000',
     'https://literate-space-rotary-phone-5pvr5xv749ghpgvx-8000.app.github.dev/',
     'https://clinica-robles-sistema-deteccion.vercel.app',
+    'https://gross-barbabra-ilan-bc39a3dd.koyeb.app',
 ]
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
